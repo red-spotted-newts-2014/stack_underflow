228171d5d91022ba35974fda9a49851c9a1fcfc1
Derek <derek.siker@gmail.com>
2014-08-08 14:23:28 -0400

    updated routes to delete redundant root, added tag migration and tag model

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :questions
-
   devise_for :users
   root to: "question#index"
 
+  resources :questions
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
-  root 'questions#index'
 end
