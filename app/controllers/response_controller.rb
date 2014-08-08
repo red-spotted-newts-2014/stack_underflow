19b70b963995e6fc08ed0f466b85892daa736c93
elz <joelyawili@hotmail.com>
2014-08-08 12:03:12 -0400

    setup response controller/model/migration

---
+++ app/controllers/response_controller.rb
@@ -0,0 +1,36 @@
+class ResponseController < ActionController::Base
+
+  def new
+    @response = Response.new
+  end
+
+  def create
+    @response = Response.create(response_params)
+    render :json => @response.to_json
+  end
+
+
+  def edit
+    @response = Response.find(params[:id])
+  end
+
+  def update
+
+    @response.update(response_params)
+    if @response.save(response_params)
+      redirect_to(@response)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def response_params
+    params.require(:response).permit(:responsable_type,:body)
+  end
+end
