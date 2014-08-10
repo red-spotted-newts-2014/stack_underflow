5c5471bbe647f913e51e565f80fb56f4823bf913
Rootul <rootulp@gmail.com>
2014-08-09 23:15:09 -0400

    user test for sign in

--- spec/requests/user_registration_spec.rb
+++ spec/requests/user_registration_spec.rb
@@ -11,4 +11,20 @@ feature "user registration" do
 
     expect(page).to have_content("Welcome! You have signed up successfully.")
   end
+
+  it "allows existing user to sign in" do
+    user = User.create(:email    => "rootul@test.com",
+                                :password => "password100")
+
+    visit "/users/sign_in"
+
+    fill_in "Email",    :with => "rootul@test.com"
+    fill_in "Password", :with => "password100"
+
+    click_button "Sign in"
+
+    expect(page).to have_content("Signed in successfully.")
+
+  end
+
 end
